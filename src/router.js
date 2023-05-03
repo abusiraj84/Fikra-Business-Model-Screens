013a2601370dcb39ca3d108f4c7394e85f98d11a
Anas864 <anassar@qewam.com>
2023-05-03 11:40:24 +0300

    Create New Routes

--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import TheLanding from "./views/TheLanding.vue";
 import TheForm from "./views/TheForm.vue";
 import TheDocuments from "./views/TheDocuments.vue";
+import TheLibrary from "./views/TheLibrary.vue";
+import TheArticle from "./views/TheArticle.vue";
 
 const router = createRouter({
 	history: createWebHistory(),
@@ -10,6 +12,8 @@ const router = createRouter({
 		{ path: "/", component: TheLanding },
 		{ path: "/create", component: TheForm },
 		{ path: "/docs", component: TheDocuments },
+		{ path: "/library", component: TheLibrary },
+		{ path: "/article", children: [{ path: ":id", component: TheArticle }] },
 	],
 });
 
---
+++ src/views/TheArticle.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>ARTICLE PAGE {{ articleID }}</div>
+</template>
+
+<script setup>
+	import { useRoute } from "vue-router";
+	const route = useRoute();
+	let articleID = route.params.id;
+</script>
