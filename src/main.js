2a31bc6452e85d64d7e920dd6b46420b23211bde
abusiraj84 <abusiraj84@gmail.com>
2023-07-12 12:14:14 +0300

    update env

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ import App from "./App.vue";
 import router from "./router";
 
 const httpLink = createHttpLink({
-  uri: "https://dev-fikra-api.qewamx.com/graphql",
+  uri: import.meta.env.VITE_GRAPHQL_URL,
 });
 const cache = new InMemoryCache();
 const apolloClient = new ApolloClient({
